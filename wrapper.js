b71a7df9c747caeac847b475f7eeb993c6eefa9a
Daniel Hug <daniel@hugwebdesign.com>
2017-05-23 11:49:00 -0700

    implement NFDH algorithm

--- wrapper.js
+++ wrapper.js
@@ -3,10 +3,9 @@
 
 	var wrapper = new Wrapper({
 		parent: element in document (required),
-		cells: array (required),
-		cellWidth: number (in pixels; required),
-		cellHeight: number (in pixels; required),
-		numCols: number (optional),
+		cells: collection of elements sorted tallest first (required),
+		cellWidth: number or array of numbers (in pixels; required),
+		cellHeight: number or array of numbers (in pixels; required),
 		fixedWidth: boolean (optional)
 	});
 */
@@ -16,13 +15,8 @@ function Wrapper(options) {
 	this.cells = options.cells;
 	this.cellWidth = options.cellWidth;
 	this.cellHeight = options.cellHeight;
-
-	// defaults to true if numCols is passed otherwise false
-	this.fixedWidth = this.fixedWidth !== undefined ? this.fixedWidth :
-		options.numCols !== undefined;
-
-	// defaults to as many columns as can fit in parent
-	this.numCols = options.numCols || this.getColCount();
+	this.fixedWidth = !!options.fixedWidth;
+	this.wrapperWidth = this.getWrapperWidth();
 
 	this.render();
 
@@ -31,38 +25,58 @@ function Wrapper(options) {
 	var timer;
 
 	function handleResize() {
-		var newColCount = instance.getColCount();
-		if (newColCount !== instance.numCols) {
-			instance.numCols = newColCount;
+		var newWrapperWidth = instance.getWrapperWidth();
+		if (newWrapperWidth !== instance.wrapperWidth) {
+			instance.wrapperWidth = newWrapperWidth;
 			instance.render();
 		}
 	}
 
-	window.addEventListener('resize', function () {
+	window.addEventListener('resize', function() {
 		clearTimeout(timer);
 		timer = setTimeout(handleResize, 50);
 	});
 }
 
-Wrapper.prototype.getColCount = function() {
+Wrapper.prototype.getWrapperWidth = function() {
 	var wrapper = this.parent.parentNode;
 	var computedStyle = getComputedStyle(wrapper);
-	var contentAreaWidth = wrapper.clientWidth - 
+	return wrapper.clientWidth - 
 		(parseFloat(computedStyle.paddingLeft) + 
 		parseFloat(computedStyle.paddingRight));
-	return Math.floor(contentAreaWidth / this.cellWidth);
+};
+
+Wrapper.prototype.getColCount = function() {
+	return Math.floor(this.getWrapperWidth() / this.cellWidth);
 };
 
 Wrapper.prototype.render = function() {
-	// set parent size
-	var numRows = Math.ceil(this.cells.length / this.numCols);
-	this.parent.style.height = this.cellHeight * numRows + 'px';
-	this.parent.style.width = this.cellWidth * this.numCols + 'px';
+	var maxRowWidth = 0;
 
 	// reposition cells
+	var top = 0;
+	var left = 0;
+	var bottom = typeof this.cellHeight === 'number' ? this.cellHeight : this.cellHeight[0];
 	for (var i = 0; i < this.cells.length; i++) {
 		var style = this.cells[i].style;
-		style.left = i % this.numCols * this.cellWidth + 'px';
-		style.top = Math.floor(i / this.numCols) * this.cellHeight + 'px';
+		var cellWidth = typeof this.cellWidth === 'number' ? this.cellWidth : this.cellWidth[i];
+		
+		// if this cell is too wide for this row
+		var right = left + cellWidth;
+		if (left > 0 && right > this.wrapperWidth) {
+			left = 0;
+			top = bottom;
+			var cellHeight = typeof this.cellHeight === 'number' ? this.cellHeight : this.cellHeight[i];
+			bottom += cellHeight;
+		} else {
+			if (right > maxRowWidth) maxRowWidth = right;
+		}
+		style.left = left + 'px';
+		style.top = top + 'px';
+		left += cellWidth;
 	}
+
+	// set parent size
+	this.parent.style.height = bottom + 'px';
+	this.parent.style.width = maxRowWidth + 'px';
 };
